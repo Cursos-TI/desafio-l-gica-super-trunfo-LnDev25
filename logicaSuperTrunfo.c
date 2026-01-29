a4e6c207204856f4ab3a02394c32b9df7ba9206b
Nathan <suporteforlucas@hotmail.com>
2026-01-29 23:14:18 +0000

    Finalizando Tema 3 - Nivel Novato - Logica com IF e ELSE

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,60 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+// INICIO DO PROGRAMA - NIVEL NOVATO TEMA 3//
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+// Variaveis carta 1
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+char letra_estado = 'S' ;
+char codigo_da_carta[] = "A01" ;
+char nome_da_cidade[] = "São Paulo" ;
+unsigned long int populacao = 11451245 ;
+float area = 1521.11 ;
+float pib_valor = 829000000000.0 ;
+int pontos_turisticos = 50 ;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+// Variaveis carta 2
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+char letra_estado2 = 'R' ;
+char codigo_da_carta2[] = "B01" ;
+char nome_da_cidade2[] = "Rio de Janeiro" ;
+unsigned long int populacao2 = 6211423 ;
+float area2 = 1200.27 ;
+float pib_valor2 = 359000000000.0 ;
+int pontos_turisticos2 = 60 ;
+ 
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+// Densidade Populacional calculo 
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+float densidade_populacional1 = populacao / area ;
+float densidade_populacional2 = populacao2 / area2 ;
 
-    return 0;
+// PIB per Capita calculo 
+
+float pib_capita = (pib_valor / populacao) ;
+float pib_capita2 = (pib_valor2 / populacao2) ;
+
+
+// If e Else comparação das duas cartas usando densidade populacional
+
+printf("\n--------------- SUPER TRUNFO ---------------\n\n");
+
+printf("\nComparação de Cartas (Atributo: Densidade Populacional):\n\n");
+
+printf("Carta 1 - %s: %.2f\n", nome_da_cidade, densidade_populacional1);
+printf("Carta 2 - %s: %.2f\n", nome_da_cidade2, densidade_populacional2);
+
+if (densidade_populacional1 < densidade_populacional2) {
+    printf ("\nResultado: Carta 1 (%s) venceu!\n", nome_da_cidade);
+} else {
+    printf ("\nResultado: Carta 2 (%s) venceu!\n", nome_da_cidade2);
+}
+
+// Fim do Programa NIVEL NOVATO TEMA 3//
+
+//-------------------------------------------------------------------------------------------------------------//
+
+return 0;
+
 }
